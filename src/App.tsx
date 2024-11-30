f69ed6762ee202609ba763a7cc23e04634d3e871
Konstantin <tugay-k@mail.ru>
2024-11-30 11:11:24 +0300

    delete: unnecessary ruote

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { TestPage } from "./pages/TestPage/TestPage";
-import { Results } from "./pages/Results/Results";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TestPage />} />
-        <Route path="/results" element={<Results />} />
       </Routes>
     </BrowserRouter>
   );
