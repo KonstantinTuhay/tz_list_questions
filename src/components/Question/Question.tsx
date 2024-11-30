b91f746eeba9e14a0c74e710629334cb7df64516
Konstantin <tugay-k@mail.ru>
2024-11-30 20:27:48 +0300

    add: reselect

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,18 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setQuestions } from "../../redux/slice/questionSLice";
-import { RootState } from "../../redux/store";
 import { ProgressBar } from "../ProgressBar/ProgressBar";
 import { Options } from "../Options/Options";
 import { dataQuestions } from "../../data/dataQuestions";
 import "../../styles/Question/Question.css";
+import { selectCurrentQuestion } from "../../redux/selectors/questionSelectors";
 
 export const Question = (): JSX.Element => {
   const dispatch = useDispatch();
 
-  const { questions, currentIndex } = useSelector(
-    (state: RootState) => state.questionSLice
-  );
+  const currentQuestion = useSelector(selectCurrentQuestion);
 
   useEffect(() => {
     dispatch(setQuestions(dataQuestions));
@@ -20,14 +18,14 @@ export const Question = (): JSX.Element => {
 
   return (
     <>
-      {questions[currentIndex] ? (
+      {currentQuestion ? (
         <div className="question-container">
           <h2 className="testing-title">Тестирование</h2>
 
           <div className="questions-box">
-            <h3 className="question-text">{questions[currentIndex].text}</h3>
+            <h3 className="question-text">{currentQuestion.text}</h3>
             <ul className="options-list">
-              {questions[currentIndex].options.map((option) => {
+              {currentQuestion.options.map((option) => {
                 return <Options option={option} key={crypto.randomUUID()} />;
               })}
             </ul>
---
+++ src/redux/selectors/questionSelectors.ts
@@ -0,0 +1,23 @@
+import { createSelector } from "reselect";
+import { RootState } from "../store";
+
+// Базовый селектор для получения данных из стейта
+const selectQuestionState = (state: RootState) => state.questionSLice;
+
+// Селектор для получения текущего вопроса
+export const selectCurrentQuestion = createSelector(
+  [selectQuestionState],
+  (questionSlice) => questionSlice.questions[questionSlice.currentIndex]
+);
+
+// Селектор для получения всех вопросов
+export const selectQuestions = createSelector(
+  [selectQuestionState],
+  (questionSlice) => questionSlice.questions
+);
+
+// Селектор для получения текущего индекса
+export const selectCurrentIndex = createSelector(
+  [selectQuestionState],
+  (questionSlice) => questionSlice.currentIndex
+);
